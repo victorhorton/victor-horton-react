09673d6d696455cd554f669091955ef85fea540e
Victor Horton <vichorton5@gmail.com>
2024-06-03 21:01:59 -0500

    Add styling to Body component and type children prop

--- src/components/layout/Body.tsx
+++ src/components/layout/Body.tsx
@@ -1,3 +1,9 @@
-export default function Body({ children }) {
-  return <>{children}</>
+import { ReactNode } from 'react'
+
+export default function Body({ children }: { children: ReactNode }) {
+  return (
+    <div className="container mx-auto px-2 sm:px-4 md:px-10 lg:px-16">
+      {children}
+    </div>
+  )
 }
