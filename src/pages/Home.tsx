5f9f889f3dd2f9e74f03f1aa42038e8400986ee5
Victor Horton <vichorton5@gmail.com>
2024-06-03 21:20:58 -0500

    Add simply Home Display text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <>Home</>
+  return (
+    <>
+      <div className="pt-10">
+        <h1 className="font-display text-7xl font-semibold">
+          Hi, I'm Victor Horton
+        </h1>
+        <h2 className="font-display text-7xl font-semibold">
+          I'm a full-stack developer
+        </h2>
+      </div>
+    </>
+  )
 }
