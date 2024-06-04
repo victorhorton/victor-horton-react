9b48eb70288a9d299f8d4faf380f81527cd79079
Victor Horton <vichorton5@gmail.com>
2024-06-03 20:06:32 -0500

    Add navbar to header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,3 +1,19 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
-  return <></>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
