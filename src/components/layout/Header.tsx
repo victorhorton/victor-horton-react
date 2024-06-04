3004fbf6bc886701bd4f52625e058d4415b3cca0
Victor Horton <vichorton5@gmail.com>
2024-06-03 20:44:42 -0500

    Add minimal style to navbar

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,15 +2,15 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <nav>
-      <ul>
-        <li>
+    <nav className="w-full h-10">
+      <ul className="flex flex-row place-content-center items-center gap-4 h-10">
+        <li className="flex-initial">
           <Link to="/">Home</Link>
         </li>
-        <li>
+        <li className="flex-initial">
           <Link to="/contact">Contact</Link>
         </li>
-        <li>
+        <li className="flex-initial">
           <Link to="/projects">Projects</Link>
         </li>
       </ul>
