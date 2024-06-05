83768e1357e2abf617fdec96d297f212e1692149
Victor Horton <vichorton5@gmail.com>
2024-06-04 21:04:54 -0500

    Add glowing text component

---
+++ src/components/GlowingText.tsx
@@ -0,0 +1,50 @@
+import { useEffect } from 'react'
+
+export default function GlowingText({ children }: { children: string }) {
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      const sections = document.querySelectorAll('.color')
+      sections.forEach((section) => {
+        const rect = section.getBoundingClientRect()
+        const distance = getDistance(
+          e.clientX,
+          e.clientY,
+          rect.left + rect.width / 2,
+          rect.top + rect.height / 2
+        )
+
+        // Calculate color based on distance
+        const maxDistance = 200 // Maximum distance to affect the color
+        const proximity = Math.max(0, 1 - distance / maxDistance) // Proximity ratio (0 to 1)
+        const colorValue = Math.floor(255 * proximity) // Adjust color based on proximity
+        const color = `rgb(${colorValue}, 0, 0)` // Color transition from black to red
+        ;(section as HTMLElement).style.color = color
+      })
+    }
+
+    document.addEventListener('mousemove', handleMouseMove)
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove)
+    }
+  }, [])
+
+  const getDistance = (
+    x1: number,
+    y1: number,
+    x2: number,
+    y2: number
+  ): number => {
+    const dx = x1 - x2
+    const dy = y1 - y2
+    return Math.sqrt(dx * dx + dy * dy)
+  }
+
+  return (
+    <>
+      {[...children].map((character) => {
+        return <span className="color">{character}</span>
+      })}
+    </>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
+import GlowingText from '../components/GlowingText'
+
 export default function Home() {
   return (
     <>
       <div className="pt-10">
         <h1 className="font-display text-7xl font-semibold">
-          Hi, I'm Victor Horton
+          <GlowingText>Hi, I'm Victor Horton</GlowingText>
         </h1>
         <h2 className="font-display text-7xl font-semibold">
-          I'm a full-stack developer
+          <GlowingText>I'm a full-stack developer</GlowingText>
         </h2>
       </div>
     </>
